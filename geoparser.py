b2d2ce8268f0bd576f6041cd47d32154059e409a
sinkapoy <sharshov-ivan@yandex.ru>
2024-06-04 21:31:08 +0300

    change the structure add crop image class

--- geoparser.py
+++ geoparser.py
@@ -2,12 +2,28 @@ import numpy as np
 import shapely
 import geotiff
 
-class GeoParser():
-    epsg: str
-    coords: shapely.Polygon
+class _Image():
     image: np.ndarray
 
-    def __init__(self, image_path: str) -> None:
+    def __init__(self) -> None:
         pass
 
-    
+    def process_image(self):
+        """Will get image features. p2."""
+        pass
+
+class SourceImage(_Image):
+    epsg: str
+    coords: shapely.Polygon
+    __image_file: geotiff.GeoTiff
+
+    def __init__(self, image_path: str) -> None:
+        self.__read_tokens(image_path)
+        self.__image_file = geotiff.GeoTiff(image_path, crs_code=32637)
+
+    def __read_tokens(self, image_path: str):
+        """Will get coords of the file in the source dataset. p1. """
+        pass
+
+class CropImage(_Image):
+    pass
